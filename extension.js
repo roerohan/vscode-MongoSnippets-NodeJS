d07ea9b6bbc808cd542b7b1837e56b34bb384ed7
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-05-17 16:53:13 +0530

    feat: v1.0.4, command pallete command: Links to docs

--- extension.js
+++ extension.js
@@ -12,19 +12,24 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+	console.log('Documentation for mongoosejs will be opened in your web browser.');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let mongooseDocs = vscode.commands.registerCommand('extension.mongoosejsDocs', function () {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://mongoosejs.com/docs/api.html#Model'))
 	});
 
-	context.subscriptions.push(disposable);
+	let extensionDocs = vscode.commands.registerCommand('extension.extensionDocs', () => {
+
+		vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://github.com/roerohan/vscode-MongoSnippets-NodeJS/blob/master/README.md'))
+	});
+
+
+	context.subscriptions.push(mongooseDocs);
+	context.subscriptions.push(extensionDocs);
 }
 exports.activate = activate;
 
