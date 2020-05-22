601467dd5b13ea53913eb2fbdfaf8df642238f08
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-22 15:20:41 +0530

    feat: Incorporate settings

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import viewCollectionJson from './connect/viewJson';
 import getModelsFromFiles from './modelUtils/getModelNames';
 import seeModelsUtil from './modelUtils/seeModels';
 import completionItemProviders from './modelUtils/completionItemProviders.js';
+import settings from './utils/settings';
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -12,11 +13,12 @@ import completionItemProviders from './modelUtils/completionItemProviders.js';
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
     console.log('Mongo Snippets has been activated.');
 
+    const sourceDir = await settings();
     const mongooseLink = 'https://mongoosejs.com/docs/api.html#Model';
     const extensionLink = 'https://github.com/roerohan/vscode-MongoSnippets-NodeJS/blob/master/README.md';
 
-    let models: { [key: string]: { file: string } } = await getModelsFromFiles();
-    let providers: vscode.Disposable[] = await completionItemProviders(models);
+    let models: { [key: string]: { file: string } } = await getModelsFromFiles(sourceDir);
+    let providers: vscode.Disposable[] = await completionItemProviders(models, sourceDir);
 
     const mongooseDocs = vscode.commands.registerCommand('extension.mongoosejsDocs', () => {
         vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(mongooseLink));
@@ -26,16 +28,16 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
         vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(extensionLink));
     });
 
-    const setup = vscode.commands.registerCommand('extension.setup', () => {
-        setupBoilerplate();
+    const setup = vscode.commands.registerCommand('extension.setup', async () => {
+        await setupBoilerplate(sourceDir);
     });
 
     const viewCollections = vscode.commands.registerCommand('extension.viewCollections', async () => {
-        viewCollectionJson();
+        viewCollectionJson(sourceDir);
     });
 
     const seeModels = vscode.commands.registerCommand('extension.seeModels', async () => {
-        await seeModelsUtil(models);
+        await seeModelsUtil(models, sourceDir);
     });
 
     context.subscriptions.push(
@@ -49,9 +51,9 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 
     setInterval(async () => {
         try {
-            models = await getModelsFromFiles();
+            models = await getModelsFromFiles(sourceDir);
             providers.forEach((provider) => provider.dispose());
-            providers = await completionItemProviders(models);
+            providers = await completionItemProviders(models, sourceDir);
         } catch (err) {
             console.error(err);
         }
